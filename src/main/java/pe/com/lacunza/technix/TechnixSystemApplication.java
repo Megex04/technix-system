bd09f3ca7ea0a10b16073d279ad26227fee6d8fb
Megex04 <miantolalfa04@gmail.com>
2025-04-10 16:35:50 -0500

    Charges first commit upload base files

---
+++ src/main/java/pe/com/lacunza/technix/TechnixSystemApplication.java
@@ -0,0 +1,13 @@
+package pe.com.lacunza.technix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnixSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnixSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/lacunza/technix/TechnixSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.lacunza.technix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnixSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
