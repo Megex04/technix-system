b323f5ca5975fca21c4c1604b1f9e3c4a0104ddf
Megex04 <miantolalfa04@gmail.com>
2025-04-17 15:44:22 -0500

    feature/category-controller-impl Add CRUD endpoints to Categories and fix issues founded

--- src/main/java/pe/com/lacunza/technix/domain/entities/jpa/Category.java
+++ src/main/java/pe/com/lacunza/technix/domain/entities/jpa/Category.java
@@ -1,11 +1,13 @@
 package pe.com.lacunza.technix.domain.entities.jpa;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
@@ -24,7 +26,7 @@ public class Category {
 
     private String description;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", nullable = false)
-    private Product product;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Product> products = new ArrayList<>();
 }
